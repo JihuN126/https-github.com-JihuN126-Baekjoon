12885c41e3db2da268ac519bf87146666e42d53d
Yu Jihun <137899326+JihuN126@users.noreply.github.com>
2026-02-11 14:13:18 +0900

    [Silver III] Title: 소수 구하기, Time: 668 ms, Memory: 34684 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] Arr = new int[N+1];
+        for(int i=1;i<=N;i++) {
+            Arr[i] = i;
+        }
+        for(int i=2;i<=Math.sqrt(N);i++) {
+            for(int j=i;j<=N;j+=i) {
+                if(i==j) continue;
+                Arr[j]=0;
+            }
+        }
+        for(int i=M;i<=N;i++) {
+           if(Arr[i]!=0 && i!=1) System.out.println(Arr[i]);
+        }
+    }
+}
