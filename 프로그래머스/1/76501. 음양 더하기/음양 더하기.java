a48fd50344e42f8335e02beeea3d53b3a1c6eefd
Yu Jihun <137899326+JihuN126@users.noreply.github.com>
2026-03-29 12:08:28 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0;i<signs.length;i++) {
+            if(signs[i]) answer += absolutes[i];
+            else answer += (0-absolutes[i]);
+        }
+        return answer;
+    }
+}
