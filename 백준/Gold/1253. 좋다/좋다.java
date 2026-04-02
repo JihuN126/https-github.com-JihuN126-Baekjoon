bbf8408fae5327c4f9c2b647a998f4b136b7e3e6
Yu Jihun <137899326+JihuN126@users.noreply.github.com>
2026-04-02 14:51:19 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<N;i++) {
+            int p1=0, p2=N-1;
+            long target = arr[i];
+            while(p1<p2) {
+                if(arr[p1] + arr[p2] > target) {
+                    p2--;
+                }
+                else if(arr[p1] + arr[p2] < target) {
+                    p1++;
+                }
+                else if(arr[p1] + arr[p2] == target) {
+                    if(p1!=i && p2!=i) {
+                        answer++;
+                        break;
+                    }
+                    else if(p1==i) {
+                        p1++;
+                    }
+                    else if(p2==i) {
+                        p2--;
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
